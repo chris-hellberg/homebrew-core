d5f2447a77db6628f09161c33fc4dce6f635c783
Chris Hellberg <54506296+chris-hellberg@users.noreply.github.com>
2021-12-24 19:59:21 -0500

    Update iodine.rb
    1. Switched to zlib dependency for macOS
    2. Reverted to make install rather than manual placement of files
    3. Revised test to check for error message rather than help text.

--- Formula/iodine.rb
+++ Formula/iodine.rb
@@ -1,4 +1,3 @@
-class Iodine < Formula
   desc "Tunnel IPv4 traffic through a DNS server"
   homepage "https://code.kryo.se/iodine"
   url "https://github.com/yarrick/iodine/archive/refs/tags/v0.7.0.tar.gz"
@@ -6,18 +5,18 @@ class Iodine < Formula
   license "ISC"
   head "https://github.com/yarrick/iodine.git", branch: "master"
   depends_on "cmake" => :build
-  
-  uses_from_macos "libzip"
+
+  uses_from_macos "zlib"
 
   def install
     system "make"
-    bin.install "bin/iodine"
-    bin.install "bin/iodined"
-    man8.install "man/iodine.8"
+    system "make", "prefix=#{prefix}", "install"
   end
 
   test do
-    system "#{bin}/iodine", "-h"
-    system "#{bin}/iodined", "-h"
+    # iodine and iodined require being run as root. Match on the non-root error text, which is printed to
+    # stderr, as a successful test
+    assert_match("iodine: Run as root and you'll be happy.", pipe_output("#{sbin}/iodine google.com 2>&1"))
+    assert_match("iodined: Run as root and you'll be happy.", pipe_output("#{sbin}/iodined google.com 2>&1"))
   end
 end
